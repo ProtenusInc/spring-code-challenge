5aaf3921650024c1518e459c17c6a44f6964bed1
Bill Streckfus <bill@protenus.com>
2020-09-23 13:39:40 -0400

    Running out-of-the-box application

---
+++ src/main/java/com/protenus/codechallenge/Application.java
@@ -0,0 +1,20 @@
+package com.protenus.codechallenge;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		// Do nothing
+	}
+}
